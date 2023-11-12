d196ed447ea2eac77de5e1d48f878cf8ae4f3fca
GS_song <20003204@sju.ac.kr>
2023-11-13 00:41:53 +0900

    feat: 값을 받아서 한국 돈 형식으로 변환하는 기능 구현

--- src/main/java/christmas/util/Utils.java
+++ src/main/java/christmas/util/Utils.java
@@ -1,11 +1,21 @@
 package christmas.util;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 
 public class Utils {
 
+    private static final String DECIMAL_FORMAT = "#,###";
+    private static final String KR_CURRENT = "원";
+
     public static List<String> splitByDelimiter(String target, String delimiter) {
         return Arrays.stream(target.split(delimiter)).toList();
+    }
+
+    public static String changeKrCurrentFormat(int target) {
+        DecimalFormat decFormat = new DecimalFormat(DECIMAL_FORMAT);
+
+        return decFormat.format(target) + KR_CURRENT;
     }
 }
