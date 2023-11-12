ebc44e75f11729a3abd6afe99b4048192d15720f
GS_song <20003204@sju.ac.kr>
2023-11-12 22:10:47 +0900

    feat: 메뉴들을 enum으로 정의

---
+++ src/main/java/christmas/model/Menu.java
@@ -0,0 +1,31 @@
+package christmas.model;
+
+public enum Menu {
+    MUSHROOM_SPOOF(MenuType.APPETIZER, "양송이수프", 6000),
+    TAPAS(MenuType.APPETIZER, "타파스", 5500),
+    CAESAR_SALAD(MenuType.APPETIZER, "시저샐러드", 8000),
+
+    T_BORN_STEAK(MenuType.MAIN, "티본스테이크", 55000),
+    BBQ_RIB(MenuType.MAIN, "바비큐립", 54000),
+    SEAFOOD_PASTA(MenuType.MAIN, "해산물파스타", 35000),
+    CHRISTMAS_PASTA(MenuType.MAIN, "크리스마스파스타", 25000),
+
+    CHOCOLATE_CAKE(MenuType.DESSERT, "초코케이크", 15000),
+    ICE_CREAM(MenuType.DESSERT, "아이스크림", 5000),
+
+    ZERO_COKE(MenuType.DRINK, "제로콜라", 3000),
+    RED_WINE(MenuType.DRINK, "레드와인", 60000),
+    CHAMPAGNE(MenuType.DRINK, "샴페인", 25000),
+    ;
+
+    private final MenuType type;
+    private final String name;
+    private final int price;
+
+    Menu(MenuType type, String name, int price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+}
