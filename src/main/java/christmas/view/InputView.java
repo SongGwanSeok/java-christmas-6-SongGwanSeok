382db7d9ba06eb5c48815250c8fe4eb8aac610b4
GS_song <20003204@sju.ac.kr>
2023-11-12 23:02:44 +0900

    feat: 입력을 담당하는 InputView 기능 구현
    readDate: 방문 날짜를 입력받아 문자로 반환
    readOrders: 주문을 입력받아 문자로 반환

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,19 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String READ_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String READ_ORDERS = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public static String readDate() {
+        System.out.println(READ_DATE);
+        return Console.readLine();
+    }
+
+    public static String readOrders() {
+        System.out.println(READ_ORDERS);
+        return Console.readLine();
+    }
+}
