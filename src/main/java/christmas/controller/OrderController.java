fdf16cd94fb3277de63b789cf4b7ea0c2f265726
GS_song <20003204@sju.ac.kr>
2023-11-12 23:08:41 +0900

    feat: 주문을 받는 전체적인 과정을 담당하는 OrderController 생성
    1. 환영 문구를 출력
    2. 방문 날짜를 입력받아 visitDate를 설정
    3. 주문을 받아 Orders를 설정

---
+++ src/main/java/christmas/controller/OrderController.java
@@ -0,0 +1,27 @@
+package christmas.controller;
+
+import christmas.model.Date;
+import christmas.model.Orders;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class OrderController {
+
+    private Date visitDate;
+    private Orders orders;
+
+    public void takeOrder() {
+        OutputView.printWelcome();
+        setVisitDate();
+        setOrders();
+    }
+
+    private void setVisitDate() {
+        visitDate = new Date(InputView.readDate());
+    }
+
+    private void setOrders() {
+        orders = new Orders(InputView.readOrders());
+    }
+
+}
