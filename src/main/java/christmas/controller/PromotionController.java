e9fa03a2eaa8af903154050f57369cdbebc5bd38
GS_song <20003204@sju.ac.kr>
2023-11-14 14:37:29 +0900

    feat: PromotionController에 증정품과 할인 세팅 기능 구현

--- src/main/java/christmas/controller/PromotionController.java
+++ src/main/java/christmas/controller/PromotionController.java
@@ -1,10 +1,14 @@
 package christmas.controller;
 
+import static christmas.model.order.Menu.CHAMPAGNE;
+
 import christmas.model.order.Date;
 import christmas.model.order.Orders;
 import christmas.model.order.UserOrder;
 import christmas.model.promotion.Discount;
 import christmas.model.promotion.Present;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PromotionController {
@@ -20,11 +24,22 @@ public class PromotionController {
     }
 
     private void setPresent() {
+        Orders orders = userOrder.orders();
+        if (orders.calculateTotalCost() >= 120000) {
+            present = new Present(CHAMPAGNE, 1);
+        }
 
     }
 
     private void setDiscounts() {
-
+        Orders orders = userOrder.orders();
+        if (orders.calculateTotalCost() < 10000) {
+            this.discounts = new ArrayList<>();
+            return;
+        }
+        discounts = Arrays.stream(Discount.values())
+                .filter(discount -> discount.calculate(userOrder) != 0)
+                .toList();
     }
 
 }
