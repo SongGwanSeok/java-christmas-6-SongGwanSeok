46b381062551a5d0297f60e5d47a0864fddadb06
GS_song <20003204@sju.ac.kr>
2023-11-14 12:58:37 +0900

    feat: 프로모션을 관리하는 PromotionController 생성

---
+++ src/main/java/christmas/controller/PromotionController.java
@@ -0,0 +1,15 @@
+package christmas.controller;
+
+import christmas.model.order.UserOrder;
+
+public class PromotionController {
+
+    private UserOrder userOrder;
+
+    public void setPromotion(UserOrder userOrder) {
+    }
+
+    public void printPromotion() {
+
+    }
+}
