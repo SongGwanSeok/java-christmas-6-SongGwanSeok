70875807bc56a8d8f2631ab10d733edac0ffb51d
GS_song <20003204@sju.ac.kr>
2023-11-15 18:50:56 +0900

    test: test를 위한 메서드를 만들지 않는다는 규칙을 위한 변경

--- src/test/java/christmas/validation/OrdersValidationTest.java
+++ src/test/java/christmas/validation/OrdersValidationTest.java
@@ -13,18 +13,15 @@ import org.junit.jupiter.params.provider.ValueSource;
 @DisplayName("주문_목록_생성_예외")
 class OrdersValidationTest {
 
-    List<Order> makeOrders(String input) {
-        List<String> strings = splitByDelimiter(input, COMMA);
-        return strings.stream()
-                .map(Order::new)
-                .toList();
-    }
-
     @ParameterizedTest
     @DisplayName("생성_실패_중복_메뉴")
     @ValueSource(strings = {"타파스-1,아이스트림-2,타파스-2", "타파스-1,시저샐러드-3,바비큐립-2,시저샐러드-1"})
     void creatOrdersFailDuplicated(String input) {
-        Assertions.assertThatThrownBy(() -> OrdersValidation.validate(makeOrders(input)))
+        List<String> strings = splitByDelimiter(input, COMMA);
+        List<Order> orders = strings.stream()
+                .map(Order::new)
+                .toList();
+        Assertions.assertThatThrownBy(() -> OrdersValidation.validate(orders))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Order.WRONG_ORDER_ERROR);
     }
@@ -33,7 +30,11 @@ class OrdersValidationTest {
     @DisplayName("생성_실패_형식_오류")
     @ValueSource(strings = {"티본스테이크,1,아이스크림,2", "티본스테이크-1-아이스크림-2"})
     void creatOrdersFailWrongFormat(String input) {
-        Assertions.assertThatThrownBy(() -> OrdersValidation.validate(makeOrders(input)))
+        List<String> strings = splitByDelimiter(input, COMMA);
+        List<Order> orders = strings.stream()
+                .map(Order::new)
+                .toList();
+        Assertions.assertThatThrownBy(() -> OrdersValidation.validate(orders))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Order.WRONG_ORDER_ERROR);
     }
@@ -42,7 +43,11 @@ class OrdersValidationTest {
     @DisplayName("생성_실패_20개_초과_메뉴_주문")
     @ValueSource(strings = {"타파스-10,아이스트림-20", "타파스-10,시저샐러드-10,바비큐립-1"})
     void creatOrdersFailOverQuantity(String input) {
-        Assertions.assertThatThrownBy(() -> OrdersValidation.validate(makeOrders(input)))
+        List<String> strings = splitByDelimiter(input, COMMA);
+        List<Order> orders = strings.stream()
+                .map(Order::new)
+                .toList();
+        Assertions.assertThatThrownBy(() -> OrdersValidation.validate(orders))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Order.WRONG_ORDER_ERROR);
     }
@@ -51,7 +56,11 @@ class OrdersValidationTest {
     @DisplayName("생성_실패_음료만_주문")
     @ValueSource(strings = {"제코콜라-10", "제로콜라-1,레드와인-3"})
     void creatOrdersFailOnlyDrink(String input) {
-        Assertions.assertThatThrownBy(() -> OrdersValidation.validate(makeOrders(input)))
+        List<String> strings = splitByDelimiter(input, COMMA);
+        List<Order> orders = strings.stream()
+                .map(Order::new)
+                .toList();
+        Assertions.assertThatThrownBy(() -> OrdersValidation.validate(orders))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Order.WRONG_ORDER_ERROR);
     }
