1b6ce24b5378f06ad5a7251cf063f74bb2867b40
GS_song <20003204@sju.ac.kr>
2023-11-15 15:42:38 +0900

    test: mockito 테스트를 사용해 UserOrder 설정 후 테스트

--- src/test/java/christmas/model/promotion/DiscountPolicyTest.java
+++ src/test/java/christmas/model/promotion/DiscountPolicyTest.java
@@ -1,56 +1,71 @@
 package christmas.model.promotion;
 
 import static christmas.model.promotion.DiscountPolicy.THIS_YEAR;
+import static org.mockito.Mockito.mock;
 
-import christmas.model.order.Date;
-import christmas.model.order.Orders;
+import christmas.model.order.MenuType;
 import christmas.model.order.UserOrder;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@DisplayName("할인_정책")
+@RunWith(MockitoJUnitRunner.class)
 class DiscountPolicyTest {
 
-    UserOrder constructUserOrder(String dateInput, String ordersInput) {
-        Date visitDate = new Date(dateInput);
-        Orders orders = new Orders(ordersInput);
-        return new UserOrder(visitDate, orders);
+    UserOrder userOrder;
+
+    @BeforeEach
+    void before() {
+        userOrder = mock(UserOrder.class);
     }
 
     @Test
     @DisplayName("디데이_할인")
     void dDayCalculate() {
-        UserOrder userOrder = constructUserOrder("25", "타파스-1");
+        int christmasDay = 25;
+        int expectDiscount = 3400;
+        Mockito.when(userOrder.checkBeforeChristmas()).thenReturn(true);
+        Mockito.when(userOrder.getDay()).thenReturn(christmasDay);
 
         Assertions.assertThat(DiscountPolicy.dDayDiscountPolicy(userOrder))
-                .isEqualTo(3400);
+                .isEqualTo(expectDiscount);
     }
 
     @Test
     @DisplayName("평일_할인")
     void weekdayCalculate() {
-        UserOrder userOrder = constructUserOrder("25", "초코케이크-1,아이스크림-2");
+        int dessertCount = 3;
+        Mockito.when(userOrder.isVisitWeekday()).thenReturn(true);
+        Mockito.when(userOrder.findMenuTypeCount(MenuType.DESSERT)).thenReturn(dessertCount);
 
         Assertions.assertThat(DiscountPolicy.weekdayDiscountPolicy(userOrder))
-                .isEqualTo(THIS_YEAR * 3);
+                .isEqualTo(THIS_YEAR * dessertCount);
     }
 
     @Test
     @DisplayName("주말_할인")
     void weekendCalculate() {
-        UserOrder userOrder = constructUserOrder("23", "티본스테이크-2");
+        int mainCount = 2;
+        Mockito.when(userOrder.isVisitWeekend()).thenReturn(true);
+        Mockito.when(userOrder.findMenuTypeCount(MenuType.MAIN)).thenReturn(mainCount);
 
         Assertions.assertThat(DiscountPolicy.weekendDiscountPolicy(userOrder))
-                .isEqualTo(THIS_YEAR * 2);
+                .isEqualTo(THIS_YEAR * mainCount);
     }
 
     @Test
     @DisplayName("특별_할인")
     void specialCalculate() {
-        UserOrder userOrder = constructUserOrder("25", "티본스테이크-2");
+        int expectDiscount = 1000;
+        Mockito.when(userOrder.isVisitStar()).thenReturn(true);
 
         Assertions.assertThat(DiscountPolicy.specialDiscountPolicy(userOrder))
-                .isEqualTo(1000);
+                .isEqualTo(expectDiscount);
     }
 
 }
