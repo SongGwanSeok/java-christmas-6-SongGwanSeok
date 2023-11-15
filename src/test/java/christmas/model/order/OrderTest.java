d5d16187470be459be34e7fb03c081876c8e2be4
GS_song <20003204@sju.ac.kr>
2023-11-15 17:09:33 +0900

    test: 주문 금액 계산 테스트

--- src/test/java/christmas/model/order/OrderTest.java
+++ src/test/java/christmas/model/order/OrderTest.java
@@ -1,5 +1,7 @@
 package christmas.model.order;
 
+import static christmas.model.order.Menu.T_BORN_STEAK;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,8 +13,20 @@ class OrderTest {
     @DisplayName("생성_성공")
     void creatOrderSuccess() {
         String input = "티본스테이크-1";
+
         Assertions.assertThatCode(() -> new Order(input))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("주문_금액_계산")
+    void calculateOrderPrice() {
+        String input = "티본스테이크-2";
+        Order order = new Order(input);
+
+        Assertions.assertThat(order.calculateOrderPrice())
+                .isEqualTo(T_BORN_STEAK.calculatePriceByQuantity(2));
+
     }
 
 
