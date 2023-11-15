cbc9f10838303e2dbde33aae661b99bd1a9c550e
GS_song <20003204@sju.ac.kr>
2023-11-15 13:58:08 +0900

    refactor: Orders에서 예외처리 로직 분리

--- src/main/java/christmas/model/order/Orders.java
+++ src/main/java/christmas/model/order/Orders.java
@@ -1,10 +1,9 @@
 package christmas.model.order;
 
-import static christmas.model.order.MenuType.DRINK;
 import static christmas.model.order.MenuType.isEqualType;
-import static christmas.model.order.Order.WRONG_ORDER_ERROR;
 import static christmas.util.StringUtils.splitByDelimiter;
 
+import christmas.validate.OrdersValidation;
 import java.util.List;
 
 public class Orders {
@@ -16,7 +15,7 @@ public class Orders {
 
     public Orders(String ordersInput) {
         List<Order> orders = makeOrders(ordersInput);
-        validate(orders);
+        OrdersValidation.validate(orders);
         this.orders = List.copyOf(orders);
     }
 
@@ -44,50 +43,5 @@ public class Orders {
         return strings.stream()
                 .map(Order::new)
                 .toList();
-    }
-
-    private void validate(List<Order> orders) {
-        isDuplicate(orders);
-        isSumBelowLimit(orders);
-        isOnlyDrink(orders);
-    }
-
-    private void isDuplicate(List<Order> orders) {
-        List<String> distinctMenuNames = getDistinctMenuNames(orders);
-
-        if (orders.size() != distinctMenuNames.size()) {
-            throw new IllegalArgumentException(WRONG_ORDER_ERROR);
-        }
-    }
-
-    private void isSumBelowLimit(List<Order> orders) {
-        if (getTotalQuantity(orders) > MAX_TOTAL_QUANTITY) {
-            throw new IllegalArgumentException(WRONG_ORDER_ERROR);
-        }
-    }
-
-    private void isOnlyDrink(List<Order> orders) {
-        if (checkAllDrink(orders)) {
-            throw new IllegalArgumentException(WRONG_ORDER_ERROR);
-        }
-    }
-
-    private static List<String> getDistinctMenuNames(List<Order> orders) {
-
-        return orders.stream()
-                .map(Order::getMenuName)
-                .distinct()
-                .toList();
-    }
-
-    private static Integer getTotalQuantity(List<Order> orders) {
-        return orders.stream()
-                .map(Order::getQuantity)
-                .reduce(0, Integer::sum);
-    }
-
-    private static boolean checkAllDrink(List<Order> orders) {
-        return orders.stream()
-                .allMatch(order -> isEqualType(order.getMenuType(), DRINK));
     }
 }
---
+++ src/main/java/christmas/validate/OrdersValidation.java
@@ -0,0 +1,57 @@
+package christmas.validate;
+
+import static christmas.model.order.MenuType.DRINK;
+import static christmas.model.order.MenuType.isEqualType;
+import static christmas.model.order.Order.WRONG_ORDER_ERROR;
+import static christmas.model.order.Orders.MAX_TOTAL_QUANTITY;
+
+import christmas.model.order.Order;
+import java.util.List;
+
+public class OrdersValidation {
+
+    public static void validate(List<Order> orders) {
+        isDuplicate(orders);
+        isSumBelowLimit(orders);
+        isOnlyDrink(orders);
+    }
+
+    private static void isDuplicate(List<Order> orders) {
+        List<String> distinctMenuNames = getDistinctMenuNames(orders);
+
+        if (orders.size() != distinctMenuNames.size()) {
+            throw new IllegalArgumentException(WRONG_ORDER_ERROR);
+        }
+    }
+
+    private static void isSumBelowLimit(List<Order> orders) {
+        if (getTotalQuantity(orders) > MAX_TOTAL_QUANTITY) {
+            throw new IllegalArgumentException(WRONG_ORDER_ERROR);
+        }
+    }
+
+    private static void isOnlyDrink(List<Order> orders) {
+        if (checkAllDrink(orders)) {
+            throw new IllegalArgumentException(WRONG_ORDER_ERROR);
+        }
+    }
+
+    private static List<String> getDistinctMenuNames(List<Order> orders) {
+
+        return orders.stream()
+                .map(Order::getMenuName)
+                .distinct()
+                .toList();
+    }
+
+    private static Integer getTotalQuantity(List<Order> orders) {
+        return orders.stream()
+                .map(Order::getQuantity)
+                .reduce(0, Integer::sum);
+    }
+
+    private static boolean checkAllDrink(List<Order> orders) {
+        return orders.stream()
+                .allMatch(order -> isEqualType(order.getMenuType(), DRINK));
+    }
+}
