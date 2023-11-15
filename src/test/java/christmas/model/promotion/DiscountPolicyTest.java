97e83c7cadd1fc03f2b05f84255e01cb5b521cfd
GS_song <20003204@sju.ac.kr>
2023-11-15 14:41:35 +0900

    test: 공통된 UserOrder 생성 규칙 메서드 분리

--- src/test/java/christmas/model/promotion/DiscountPolicyTest.java
+++ src/test/java/christmas/model/promotion/DiscountPolicyTest.java
@@ -11,13 +11,16 @@ import org.junit.jupiter.api.Test;
 
 class DiscountPolicyTest {
 
+    UserOrder constructUserOrder(String dateInput, String ordersInput) {
+        Date visitDate = new Date(dateInput);
+        Orders orders = new Orders(ordersInput);
+        return new UserOrder(visitDate, orders);
+    }
 
     @Test
     @DisplayName("디데이_할인")
     void dDayCalculate() {
-        Date visitDate = new Date("25");
-        Orders orders = new Orders("타파스-1");
-        UserOrder userOrder = new UserOrder(visitDate, orders);
+        UserOrder userOrder = constructUserOrder("25", "타파스-1");
 
         Assertions.assertThat(DiscountPolicy.dDayDiscountPolicy(userOrder))
                 .isEqualTo(3400);
@@ -26,9 +29,7 @@ class DiscountPolicyTest {
     @Test
     @DisplayName("평일_할인")
     void weekdayCalculate() {
-        Date visitDate = new Date("25");
-        Orders orders = new Orders("초코케이크-1,아이스크림-2");
-        UserOrder userOrder = new UserOrder(visitDate, orders);
+        UserOrder userOrder = constructUserOrder("25", "초코케이크-1,아이스크림-2");
 
         Assertions.assertThat(DiscountPolicy.weekdayDiscountPolicy(userOrder))
                 .isEqualTo(THIS_YEAR * 3);
@@ -37,9 +38,7 @@ class DiscountPolicyTest {
     @Test
     @DisplayName("주말_할인")
     void weekendCalculate() {
-        Date visitDate = new Date("23");
-        Orders orders = new Orders("티본스테이크-2");
-        UserOrder userOrder = new UserOrder(visitDate, orders);
+        UserOrder userOrder = constructUserOrder("23", "티본스테이크-2");
 
         Assertions.assertThat(DiscountPolicy.weekendDiscountPolicy(userOrder))
                 .isEqualTo(THIS_YEAR * 2);
@@ -48,9 +47,7 @@ class DiscountPolicyTest {
     @Test
     @DisplayName("특별_할인")
     void specialCalculate() {
-        Date visitDate = new Date("25");
-        Orders orders = new Orders("티본스테이크-2");
-        UserOrder userOrder = new UserOrder(visitDate, orders);
+        UserOrder userOrder = constructUserOrder("25", "티본스테이크-2");
 
         Assertions.assertThat(DiscountPolicy.specialDiscountPolicy(userOrder))
                 .isEqualTo(1000);
