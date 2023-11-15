dd0eccbf511a9875573249681eb2e4daa7977f06
GS_song <20003204@sju.ac.kr>
2023-11-15 17:06:37 +0900

    test: 총 주문 금액 계산과 타입 갯수 계산 테스트

--- src/test/java/christmas/model/order/OrdersTest.java
+++ src/test/java/christmas/model/order/OrdersTest.java
@@ -1,19 +1,45 @@
 package christmas.model.order;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("주문_내역")
 class OrdersTest {
 
-    @Test
-    @DisplayName("생성_성공")
-    void creatOrdersSuccess() {
+    private Orders orders;
+
+    @BeforeEach
+    void before() {
         String input = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
-        Assertions.assertThatCode(() -> new Orders(input))
-                .doesNotThrowAnyException();
+        orders = new Orders(input);
     }
 
+    @Test
+    @DisplayName("총_주문_금액_계산")
+    void calculateTotalCost() {
+        int expectTotalCost = 142000;
+        Assertions.assertThat(orders.calculateTotalCost())
+                .isEqualTo(expectTotalCost);
+    }
+
+    @Test
+    @DisplayName("주문에서_주어진_메뉴_타입과_동일한_타입을_가지는_주문_갯수_찾기")
+    void findMenuTypeCount() {
+        int appetizerCount = 0;
+        int mainCount = 2;
+        int dessertCount = 2;
+        int drinkCount = 1;
+        Assertions.assertThat(orders.findMenuTypeCount(MenuType.APPETIZER))
+                .isEqualTo(appetizerCount);
+        Assertions.assertThat(orders.findMenuTypeCount(MenuType.MAIN))
+                .isEqualTo(mainCount);
+        Assertions.assertThat(orders.findMenuTypeCount(MenuType.DESSERT))
+                .isEqualTo(dessertCount);
+        Assertions.assertThat(orders.findMenuTypeCount(MenuType.DRINK))
+                .isEqualTo(drinkCount);
+
+    }
 
 }
